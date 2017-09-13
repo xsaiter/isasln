4834316c993beca22288ddb84ca4faa7e6c3fe69
xsaiter <xsaiter@mail.ru>
2017-09-14 01:34:06 +0300

    improve

--- isa/strings/aho_corasick.cc
+++ isa/strings/aho_corasick.cc
@@ -12,7 +12,7 @@ std::vector<aho_corasick_t::result_t>
 aho_corasick_t::search(const std::string &text) const {
   std::vector<aho_corasick_t::result_t> result;
 
-  node_ptr_t current = root_;
+  node_ptr_s current = root_;
   const std::size_t len = text.size();
 
   for (std::size_t i = 0; i < len; ++i) {
@@ -21,7 +21,7 @@ aho_corasick_t::search(const std::string &text) const {
 
     while (loop) {
       loop = false;
-      node_ptr_t next = next_state(current, c);
+      node_ptr_s next = next_state(current, c);
       if (next) {
         current = next;
         append(result, current, i);
@@ -52,14 +52,14 @@ void aho_corasick_t::build_go() {
   root_->parent = root_;
 
   for (const std::string &pattern : patterns_) {
-    node_ptr_t current = root_;
+    node_ptr_s current = root_;
 
     for (const char c : pattern) {
-      node_ptr_t next = next_state(current, c);
+      node_ptr_s next = next_state(current, c);
       if (next) {
         current = next;
       } else {
-        node_ptr_t node = make_node(c, n++, current);
+        node_ptr_s node = make_node(c, n++, current);
         current->children.push_back(node);
         current = node;
       }
@@ -71,25 +71,25 @@ void aho_corasick_t::build_go() {
 }
 
 void aho_corasick_t::build_failure() {
-  std::queue<node_ptr_t> q;
+  std::queue<node_ptr_s> q;
   bool first = true;
 
-  node_ptr_t node = root_;
+  node_ptr_s node = root_;
   q.push(node);
 
   while (!q.empty()) {
     node = q.front();
     q.pop();
 
-    for (node_ptr_t child : node->children) {
+    for (node_ptr_s child : node->children) {
       q.push(child);
       if (first) {
         child->failure = root_;
         first = false;
       } else {
-        std::vector<node_ptr_t> path;
+        std::vector<node_ptr_s> path;
 
-        node_ptr_t q = child;
+        node_ptr_s q = child;
         path.push_back(q);
 
         while (q->parent != q) {
@@ -104,7 +104,7 @@ void aho_corasick_t::build_failure() {
         const std::size_t path_len = path.size();
 
         for (std::size_t i = 0; i < path_len; ++i) {
-          for (node_ptr_t p : q->children) {
+          for (node_ptr_s p : q->children) {
             if (p != path[i] && p->c == path[i]->c) {
               q = p;
               break;
@@ -120,27 +120,27 @@ void aho_corasick_t::build_failure() {
   root_->failure = root_;
 }
 
-aho_corasick_t::node_ptr_t aho_corasick_t::next_state(node_ptr_t node,
+aho_corasick_t::node_ptr_s aho_corasick_t::next_state(node_ptr_s node,
                                                       char c) const {
   auto found = std::find_if(node->children.begin(), node->children.end(),
-                            [&](node_ptr_t n) { return n->c == c; });
+                            [&](node_ptr_s n) { return n->c == c; });
   if (found != node->children.end()) {
     return *found;
   }
   return nullptr;
 }
 
-aho_corasick_t::node_ptr_t
+aho_corasick_t::node_ptr_s
 aho_corasick_t::make_node(char c, unsigned int n,
-                          aho_corasick_t::node_ptr_t parent) const {
-  node_ptr_t node = std::make_shared<node_t>();
+                          aho_corasick_t::node_ptr_s parent) const {
+  node_ptr_s node = std::make_shared<node_t>();
   node->c = c;
   node->n = n;
   node->parent = parent;
   return node;
 }
 
-void aho_corasick_t::append(std::vector<result_t> &result, node_ptr_t node,
+void aho_corasick_t::append(std::vector<result_t> &result, node_ptr_s node,
                             std::size_t end) const {
   if (node->has_output) {
     result.emplace_back(node->output, end);
--- isa/strings/aho_corasick.hh
+++ isa/strings/aho_corasick.hh
@@ -19,26 +19,26 @@ public:
 
 private:
   struct node_t;
-  using node_ptr_t = std::shared_ptr<node_t>;
+  using node_ptr_s = std::shared_ptr<node_t>;
 
   struct node_t {
     char c;
     unsigned int n;
-    node_ptr_t parent;
-    std::vector<node_ptr_t> children;
-    node_ptr_t failure;
+    node_ptr_s parent;
+    std::vector<node_ptr_s> children;
+    node_ptr_s failure;
     bool has_output;
     std::string output;
   };
 
   std::vector<std::string> patterns_;
-  node_ptr_t root_;
+  node_ptr_s root_;
 
   void build_machine();
   void build_go();
   void build_failure();
-  node_ptr_t next_state(node_ptr_t node, char c) const;
-  node_ptr_t make_node(char c, unsigned int n, node_ptr_t parent) const;
-  void append(std::vector<result_t> &result, node_ptr_t node,
+  node_ptr_s next_state(node_ptr_s node, char c) const;
+  node_ptr_s make_node(char c, unsigned int n, node_ptr_s parent) const;
+  void append(std::vector<result_t> &result, node_ptr_s node,
               std::size_t end) const;
 };
