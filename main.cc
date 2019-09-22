b391407c34f4402248b47fd6e11016353e9117d0
isa <isa@isa>
2019-09-22 15:00:41 +0300

    q

--- main.cc
+++ main.cc
@@ -10,7 +10,7 @@ int main(int argc, char *argv[]) {
   std::vector<int> v;
   isa::ar::Is_prime_num<int> nums(3, 20);
   for (int x : nums) {
-    v.push_back(x);
+    v.push_back(x);;
   }
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
