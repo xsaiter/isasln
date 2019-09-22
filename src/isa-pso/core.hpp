a7fe79ee314157892ce0d82b109c88643cbc4534
xsaiter <xsaiter@mail.ru>
2019-09-22 20:57:36 +0300

    cmake

--- src/isa-pso/core.hpp
+++ src/isa-pso/core.hpp
@@ -12,8 +12,8 @@ using Sln = Single_linked_list_node;
 using Slnp = std::shared_ptr<Single_linked_list_node>;
 
 Slnp make_sln(int data, Slnp next) {
-  Slnp result(new Sln{data, next});
-  return result;
+  Slnp res(new Sln{data, next});
+  return res;
 }
 
 bool is_leap_year(int year) {
