2c85ee1679fb11e55a11ad8697862ba65e821093
xsaiter <xsaiter@mail.ru>
2019-09-29 23:43:47 +0300

    improve

--- src/isa-pso/main.cpp
+++ src/isa-pso/main.cpp
@@ -2,11 +2,15 @@
 #include "isa/sort.hpp"
 #include "lia/matrix.hpp"
 
+#include "rb_tree.hpp"
+
 int main(int argc, char const *argv[]) {
   std::vector<int> v{100, 5, 3, 13, 23};
   std::string s;
   isa::lia::Matrix<int> m(5, 5);
+
   auto elems = m * m;
+
   isa::sorting::bubble_sort(v.begin(), v.end());
   return 0;
 }
