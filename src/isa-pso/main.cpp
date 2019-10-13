2fe1ad36a215faeb53072f7b66bca7eb77cf15cf
xsaiter <xsaiter@mail.ru>
2019-10-13 11:35:09 +0300

    improve

--- src/isa-pso/main.cpp
+++ src/isa-pso/main.cpp
@@ -9,14 +9,13 @@
 #include "utils.hpp"
 
 int main(int argc, char const *argv[]) {
-  constexpr int n = 4;
-  constexpr int m = 5;
-  bool a[n][m];
-  pso::shen::t_01::T_01_02_09 t;
-  t.run<4, 5>(a);
-  pso::msh::t_08::A_nosum x;
-  std::vector<std::string> ss;
-  std::vector<int> v{1, 2, 4, 8};
-  auto res = x.run(v);
+  int i = 2;
+  int n = 10;
+  while (i > -1) {
+    if (i == n - 1) {
+      break;
+    }
+    i = i + (n - i) / 2;
+  }
   return 0;
 }
--- src/isa-pso/shen/shen_01.hpp
+++ src/isa-pso/shen/shen_01.hpp
@@ -38,7 +38,6 @@ struct T_01_02_09 {
   template <int n, int m> int run(bool a[n][m]) {
     int res = 0;
     bool state = false;
-
     for (auto i = 1; i <= n; ++i) {
       for (auto j = 0; j < m; ++j) {
         if ((i < n && !a[i][j]) || i == n) {
@@ -52,8 +51,52 @@ struct T_01_02_09 {
         }
       }
     }
-
     return res;
+  }
+};
+
+struct T_01_02_28 {
+  template <int n, int m> std::tuple<int, int> run(int a[n][m], int x) {
+    int l = 0;
+    int r = n - 1;
+    int k = -1;
+
+    while (l <= r) {
+      k = l + (r - l) / 2;
+      auto p = a[0][k];
+      auto q = a[n - 1][k];
+      if (p == x) {
+        return std::tuple(0, k);
+      }
+      if (q == x) {
+        return std::tuple(n - 1, k);
+      }
+      if (x > p && x < q) {
+        break;
+      } else if (x > q) {
+        l = k + 1;
+      } else if (x < p) {
+        r = k - 1;
+      }
+    }
+
+    if (l <= r && k != -1) {
+      l = 0;
+      r = m - 1;
+      while (l <= r) {
+        auto t = l + (r - l) / 2;
+        if (a[k][t] == x) {
+          return std::tuple(l, r);
+        }
+        if (a[k][t] < x) {
+          l = t + 1;
+        } else {
+          r = t - 1;
+        }
+      }
+    }
+
+    return std::tuple(-1, -1);
   }
 };
 } // namespace pso::shen::t_01
