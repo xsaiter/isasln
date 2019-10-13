b3dbd4a4fc0bbd300755680fdd9696f33fed5452
xsaiter <xsaiter@mail.ru>
2019-10-13 23:30:26 +0300

    improve

--- src/isa-pso/main.cpp
+++ src/isa-pso/main.cpp
@@ -5,17 +5,44 @@
 #include <string>
 
 #include "msh/t_08.hpp"
+#include "msh/t_15.hpp"
 #include "shen/shen_01.hpp"
 #include "utils.hpp"
 
 int main(int argc, char const *argv[]) {
-  int i = 2;
-  int n = 10;
-  while (i > -1) {
-    if (i == n - 1) {
-      break;
-    }
-    i = i + (n - i) / 2;
+
+  int n = 100;
+  auto ttttt = n % 1000;
+  int i = 1;
+  int x = n;
+  std::vector<int> nums;
+  while (x > 0) {
+    x = n % (10 * i);
+    nums.push_back(x);
+    x = n;
+    ++i;
   }
+  pso::msh::t_15::F_polymul f;
+  using Term = pso::msh::t_15::F_polymul::Term;
+  using Terms = pso::msh::t_15::F_polymul::Terms;
+  Term t1;
+  t1.factor = 10;
+  t1.degree = 3;
+
+  Term t2;
+  t2.factor = 20;
+  t2.degree = 3;
+
+  Term t3;
+  t3.factor = 5;
+  t3.degree = 4;
+
+  Term t4;
+  t4.factor = 15;
+  t4.degree = 4;
+
+  Terms terms{t1, t2, t3, t4};
+
+  auto r = f.parse_terms("10x^5+20x^4");
   return 0;
 }
--- src/isa-pso/msh/t_15.hpp
+++ src/isa-pso/msh/t_15.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <algorithm>
+#include <cassert>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -8,16 +9,16 @@
 namespace pso::msh::t_15 {
 struct F_polymul {
   struct Term {
-    bool add;
     int factor;
     int degree;
   };
 
   using Terms = std::vector<Term>;
+  using Size = std::size_t;
 
   std::string run(const std::string &x, const std::string &y) {
-    auto tx = split_to_terms(x);
-    auto ty = split_to_terms(y);
+    auto tx = parse_terms(x);
+    auto ty = parse_terms(y);
     auto tr = mul(tx, ty);
     auto res = write_to_str(reduce(tr));
     return res;
@@ -26,50 +27,124 @@ struct F_polymul {
   std::string write_to_str(const Terms &ts) {
     std::ostringstream ss;
     for (const Term &t : ts) {
-      ss << t.add ? "+" : "-";
       ss << t.factor;
       if (t.degree > 0) {
         ss << "x";
         if (t.degree > 1) {
-          ss << t.degree;
+          ss << "^" << t.degree;
         }
       }
     }
-    std::string res = ss.str();
-    return res;
+    return ss.str();
   }
 
   Terms reduce(Terms &ts) {
-    Terms r;
+    Terms res;
     std::sort(std::begin(ts), std::end(ts),
               [](const Term &x, const Term &y) { return x.degree > y.degree; });
-
-    return r;
+    const Size n = ts.size();
+    Size i = 1;
+    Term ta = ts[0];
+    while (i < n) {
+      if (ta.degree == ts[i].degree) {
+        ta = add(ta, ts[i]);
+      } else {
+        res.push_back(ta);
+        ta = ts[i];
+      }
+      ++i;
+    }
+    res.push_back(ta);
+    return res;
   }
 
   std::vector<Term> mul(const Terms &x, const Terms &y) {
     std::vector<Term> r;
-    const auto nx = x.size();
-    const auto ny = y.size();
-    for (std::size_t i = 0; i < nx; ++i) {
-      for (std::size_t j = 0; j < ny; ++j) {
-        auto t = mul(x[i], y[j]);
-        r.push_back(t);
+    const Size nx = x.size();
+    const Size ny = y.size();
+    for (Size i = 0; i < nx; ++i) {
+      for (Size j = 0; j < ny; ++j) {
+        r.push_back(mul(x[i], y[j]));
       }
     }
     return r;
   }
 
   Term mul(const Term &x, const Term &y) {
-    Term r;
-    r.add = x.add && y.add;
-    r.factor = x.factor * y.factor;
-    r.degree = x.degree + y.degree;
-    return r;
+    Term res;
+    res.factor = x.factor * y.factor;
+    res.degree = x.degree + y.degree;
+    return res;
   }
 
-  Terms split_to_terms(const std::string &s) {
+  Term add(const Term &x, const Term &y) {
+    assert(x.degree == y.degree);
+    Term res;
+    res.factor = x.factor + y.factor;
+    res.degree = x.degree;
+    return res;
+  }
+
+  Terms parse_terms(const std::string &s) {
     Terms res;
+    const Size n = s.size();
+    Size i = 0;
+    int state = 1;
+    bool add = true;
+    Term t;
+    bool tc = false;
+    while (i < n) {
+      char c = s[i];
+      switch (state) {
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      }
+      t.factor = 1;
+      t.degree = 0;
+      if (c == '+') {
+        add = true;
+        state = 1;
+      }
+      if (c == '-') {
+        add = false;
+        state = 1;
+      }
+      if (c == 'x') {
+        state = 3;
+      }
+      if (std::isdigit(c)) {
+        if (state == 3) {
+          state = 4;
+        }
+        state = true;
+        ++i;
+        std::ostringstream oss;
+        oss << c;
+        while (i < n && std::isdigit(s[i])) {
+          oss << s[i];
+          ++i;
+        }
+        auto num = std::atoi(oss.str().c_str());
+        if (!add) {
+          num *= (-1);
+        }
+        if (state == 4) {
+          t.factor = num;
+        } else {
+          t.degree = num;
+        }
+      }
+      ++i;
+    }
+    if (tc) {
+      res.push_back(t);
+    }
     return res;
   }
 };
