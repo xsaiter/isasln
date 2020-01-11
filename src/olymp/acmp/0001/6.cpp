87900bc73fc8b92f33d3adb830fd26db6aa4cb1f
xsaiter <xsaiter@mail.ru>
2020-01-12 01:54:21 +0300

    6.cpp

---
+++ src/olymp/acmp/0001/6.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ui = unsigned int;
+
+int main() {
+  auto err = []() { cout << "ERROR" << endl; };
+  auto yes = []() { cout << "YES" << endl; };
+  auto no = []() { cout << "NO" << endl; };
+  constexpr ui expected_len = 5;
+  string s;
+  cin >> s;
+  const ui len = s.size();
+  if (len != expected_len || s[2] != '-' || s[0] < 'A' || s[0] > 'H' ||
+      s[3] < 'A' || s[3] > 'H' || s[1] < '1' || s[1] > '8' || s[4] < '1' ||
+      s[4] > '8') {
+    err();
+  } else {
+    auto w = abs(s[0] - s[3]);
+    auto h = abs(s[1] - s[4]);
+    if ((w == 2 && h == 1) || (w == 1 && h == 2)) {
+      yes();
+    } else {
+      no();
+    }
+  }
+  return 0;
+}
