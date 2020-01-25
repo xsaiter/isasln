cbf731f52de52a2b9eebaf80b73a894c06c3d6c4
xsaiter <xsaiter@mail.ru>
2020-01-26 00:17:02 +0300

    acmp 7

--- src/olymp/acmp/0001/7.cpp
+++ src/olymp/acmp/0001/7.cpp
@@ -2,4 +2,58 @@
 
 using namespace std;
 
-int main() { return 0; }
+int cmp(const string &a, const string &b) {
+  auto na = a.size();
+  auto nb = b.size();
+  if (na > nb) {
+    return -1;
+  }
+  if (na < nb) {
+    return 1;
+  }
+  for (int i = 0; i < na; ++i) {
+    if (a[i] > b[i]) {
+      return -1;
+    } else if (a[i] < b[i]) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int main() {
+  string a, b, c;
+  cin >> a >> b >> c;
+  string r;
+  auto x = cmp(a, b);
+  if (x == -1) {
+    x = cmp(a, c);
+    if (x == -1) {
+      r = a;
+    } else if (x == 1) {
+      r = c;
+    } else {
+      r = a;
+    }
+  } else if (x == 1) {
+    x = cmp(b, c);
+    if (x == -1) {
+      r = b;
+    } else if (x == 1) {
+      r = c;
+    } else {
+      r = b;
+    }
+  } else {
+    x = cmp(a, c);
+    if (x == 0) {
+      r = a;
+    } else if (x == -1) {
+      r = a;
+    } else {
+      r = c;
+    }
+  }
+  cout << r << endl;
+  return 0;
+}
