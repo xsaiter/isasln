fe7bef07b3fc6307096bde75ef2f1f5b6518596e
xsaiter <xsaiter@mail.ru>
2020-02-01 17:48:37 +0300

    sync_with_stdio

--- src/olymp/acmp/0001/41.cpp
+++ src/olymp/acmp/0001/41.cpp
@@ -7,18 +7,20 @@ using namespace std;
 int b[N];
 
 int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
   int n, v;
   cin >> n;
   for (int i = 0; i < n; ++i) {
-    scanf("%d", &v);
+    cin >> v;
     ++(b[v + 100]);
   }
   for (int i = 0; i < N; ++i) {
     v = b[i];
     for (int j = 0; j < v; ++j) {
-      printf("%d ", i - 100);
+      cout << i - 100 << " ";
     }
   }
-  fflush(stdout);
+  cout << endl;
   return 0;
 }
