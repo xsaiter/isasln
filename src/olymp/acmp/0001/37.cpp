8c214e162cd1f08b3434741098c7a95f4dfc053d
xsaiter <xsaiter@mail.ru>
2020-02-07 01:29:05 +0300

    acmp 44

--- src/olymp/acmp/0001/37.cpp
+++ src/olymp/acmp/0001/37.cpp
@@ -5,15 +5,8 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  string s = "abcab";
-  int n = s.size();
-  set<char> se;
-  for (int i = 0; i < n; ++i) {
-  }
-
-  /*int n;
+  int n;
   double q;
-  cin >> n >> q;*/
+  cin >> n >> q;
   return 0;
 }
--- src/olymp/acmp/0001/44.cpp
+++ src/olymp/acmp/0001/44.cpp
@@ -2,21 +2,37 @@
 
 using namespace std;
 
-// '>>-->' '<--<<'
 int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
   string s;
   cin >> s;
-  int n = static_cast<int>(s.size());
-  int res = 0;
-  int r = 0;
-  for (int i = 0 i < n; ++i) {
-    if (s[i] == '>') {
-      if (r == 0) {
-        r = 1;
-      } else if (r == 1) {
-        r == 2;
-      } else {
+
+  string r = ">>-->";
+  string l = "<--<<";
+
+  const size_t n = s.size();
+  const size_t nr = r.size();
+  const size_t nl = l.size();
+
+  int res = 0, i, j;
+
+  for (i = 0; i < n; ++i) {
+    for (j = 0; j < nr; ++j) {
+      if (s[i + j] != r[j]) {
+        break;
       }
+    }
+    if (j == nr) {
+      ++res;
+    }
+    for (j = 0; j < nl; ++j) {
+      if (s[i + j] != l[j]) {
+        break;
+      }
+    }
+    if (j == nl) {
+      ++res;
     }
   }
   cout << res << endl;
