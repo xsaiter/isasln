c93505004e21fc36ddd17c5f532473394e892a01
xsaiter <xsaiter@mail.ru>
2020-02-10 00:09:04 +0300

    acmp

---
+++ src/olymp/acmp/0002/54.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, m;
+  cin >> n >> m;
+  n++;
+  vector<vector<int>> a(n, vector<int>(m, 0));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+    }
+  }
+  return 0;
+}
