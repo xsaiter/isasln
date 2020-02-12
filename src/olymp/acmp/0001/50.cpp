034d09af7843eec68c5b3fb59887719b64b85372
xsaiter <xsaiter@mail.ru>
2020-02-13 01:04:28 +0300

    acmp 50

--- src/olymp/acmp/0001/50.cpp
+++ src/olymp/acmp/0001/50.cpp
@@ -5,6 +5,30 @@ using namespace std;
 int main() {
   ios::sync_with_stdio(0);
   cin.tie(0);
-
+  string a, b;
+  cin >> a >> b;
+  const int n = a.size();
+  const int m = b.size();
+  int res = 0;
+  if (n >= m) {
+    for (int i = 0; i <= n - m; ++i) {
+      bool ok = false;
+      int j;
+      for (int k = 0; k < m && !ok; ++k) {
+        for (j = 0; j < m && !ok; ++j) {
+          int p = (j + k) % m;
+          if (a[i + p] != b[j]) {
+            break;
+          }
+        }
+        if (j == m) {
+          ok = true;
+          ++res;
+          break;
+        }
+      }
+    }
+  }
+  cout << res << endl;
   return 0;
 }
--- src/olymp/acmp/0002/69.cpp
+++ src/olymp/acmp/0002/69.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 constexpr double eps = 1e-3;
 
+bool less_than(double a, double b) {
+  return (b - a) > ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * eps);
+}
+
 int main() {
   int n, a;
   cin >> n >> a;
   double fi = (2.0 * 3.14159265358979323846) / (2.0 * (double)n);
-  double x = (1 / cos(fi) - tan(fi));
+  double x = 1 / cos(fi) - tan(fi);
   double res = ((double)a / 2.0) * x;
-  bool cmp = islessequal(res, 1.0);
+  bool cmp = less_than(res, 1.0);
   if (cmp) {
     cout << "YES";
   } else {
