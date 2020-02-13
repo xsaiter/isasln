c5d3bc477374a97714b7f671035a5e049effc803
xsaiter <xsaiter@mail.ru>
2020-02-14 01:06:42 +0300

    acmp

--- src/olymp/acmp/0001/20.cpp
+++ src/olymp/acmp/0001/20.cpp
@@ -11,5 +11,40 @@ int main() {
   for (int i = 0; i < n; ++i) {
     cin >> a[i];
   }
+  int max_res = 1;
+  for (int i = 0; i < n; ++i) {
+    int t = 0;
+    int res = 1;
+    for (int j = i; j < n - 1; ++j) {
+      if (a[j] < a[j + 1]) {
+        if (t == 0) {
+          t = 1;
+        } else {
+          if (t == 1) {
+            j = n - 1;
+          } else {
+            ++res;
+            t = 1;
+          }
+        }
+      } else if (a[j] > a[j + 1]) {
+        if (t == 0) {
+          t = 2;
+        } else {
+          if (t == 2) {
+            j = n - 1;
+          } else {
+            ++res;
+            t = 2;
+          }
+        }
+      }
+    }
+    if (res > max_res) {
+      max_res = res;
+    }
+  }
+  max_res++;
+  cout << max_res << endl;
   return 0;
 }
