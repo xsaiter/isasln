8596a31a4f77294a43b704a682815bea5da59720
xsaiter <xsaiter@mail.ru>
2020-06-01 23:48:33 +0300

    acmp 16

--- src/olymp/acmp/0001/16/sol.cpp
+++ src/olymp/acmp/0001/16/sol.cpp
@@ -2,14 +2,24 @@
 
 using namespace std;
 
-int n;
-
-int rec(int k){
-	return k;
+int rec(int k, int p) {
+  if (p == 0) {
+    return 1;
+  }
+  int res = 0;
+  for (int i = 1; i < k; ++i) {
+    if (p - i < 0) {
+      break;
+    }
+    res += rec(i, p - i);
+  }
+  return res;
 }
 
 int main() {
-	cin >> n;
-
-	return 0;
+  int n;
+  cin >> n;  
+  int res = rec(n + 1, n);
+  printf("%d", res);
+  return 0;
 }
--- src/olymp/acmp/0002/54/sol.cpp
+++ src/olymp/acmp/0002/54/sol.cpp
@@ -2,7 +2,18 @@
 
 using namespace std;
 
+#define MP make_pair
+#define i64 long long
+#define forn(i, n) for(int i = 0; i < n; ++i)
+#define PB push_back
+#define F first
+#define S second
+#define vi vector<int>
+#define vi64 vector<i64>
+
 int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   ios::sync_with_stdio(0);
   cin.tie(0);
   int n, m;
@@ -10,6 +21,7 @@ int main() {
   vector<vector<int>> a(n, vector<int>(m, 0));
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
+    printf("%d %d", n, m);
     }
   }
   return 0;
