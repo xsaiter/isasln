42b01dcaed402166d51c6ae3b681de798bfaebc2
xsaiter <xsaiter@mail.ru>
2020-06-05 17:02:18 +0300

    improve

--- src/olymp/codef/contests/1363/a/sol.cpp
+++ src/olymp/codef/contests/1363/a/sol.cpp
@@ -1,21 +1,43 @@
 #include <bits/stdc++.h>
 
+#define N 1001
+
+int a[N];
+bool b[N];
+int mask[N];
+
 using namespace std;
 
-bool solve(vector<int> &a, int n, int x) {
-  string mask(x, 1);
-  mask.resize(n, 0);
+void init_a() {
+  fill_n(a, N, 0);
+}
+
+void init_b() {
+  fill_n(b, N, false);
+}
+
+void init_mask(int x) {  
+  fill_n(mask, N, 0);
+  fill_n(mask, x, 1);
+}
+
+bool solve(int n, int x) {
+  init_mask(x);
+  init_b();
+  for (int i = 0; i < n; ++i) {
+    b[i] = (a[i] % 2) == 0 ? false : true;
+  }
   do {
-    int z = 0;
+    bool z = false;
     for (int i = 0; i < n; ++i) {
       if (mask[i]) {
-        z += a[i];
+        z ^= b[i];
       }
-    }            
-    if (z % 2 > 0) {
+    }
+    if (z) {
       return true;
     } 
-  } while (prev_permutation(mask.begin(), mask.end()));
+  } while (prev_permutation(mask, mask + n));
   return false;
 }
 
@@ -23,16 +45,15 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   int tt;
-  cin >> tt;
+  cin >> tt;    
   for (int t = 0; t < tt; ++t) {
     int n, x;
     cin >> n >> x;
-		vector<int> a(n);
-		for (int i = 0; i <  n; ++i) {
+    init_a();
+    for (int i = 0; i < n; ++i) {
       cin >> a[i];
-		}	
-	  bool res = solve(a, n, x);		
-		cout << (res ? "yes" : "no") << endl;
+    }	
+    cout << (solve(n, x) ? "yes" : "no") << endl;
   }
   return 0;
 }
