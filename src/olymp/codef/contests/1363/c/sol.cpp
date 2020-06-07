a18935765e57e67d72a18c1b25f33d8fe850c757
xsaiter <xsaiter@mail.ru>
2020-06-07 22:58:18 +0300

    codef 1363 c

--- src/olymp/codef/contests/1363/c/sol.cpp
+++ src/olymp/codef/contests/1363/c/sol.cpp
@@ -8,42 +8,47 @@ using namespace std;
 using G = vector<list<int>>;
 using pii = pair<int, int>;
 
-bool solve(G &g, int n, int x) {
+int solve(G &g, int n, int x) {
+  if (g.size() == 0 || g[x].size() <= 1) {
+    return 1;
+  }
   vector<pii> d(n);
   vector<bool> marked(n, false);
-  int dist = 1;
   queue<int> q;
+  marked[x] = true;
+  d[x] = {x, 0};
   q.push(x);
   while (!q.empty()) {
-    auto p = q.front();
+    auto s = q.front();
     q.pop();
-    auto jj = g[p];
-    for (auto j : jj) {
-      if (!marked[j]) {
-        marked[j] = true;
-        d[j] = {p, dist};
-        q.push(j);
+    for (auto u : g[s]) {
+      if (!marked[u]) {
+        marked[u] = true;
+        d[u] = {s, d[s].se + 1};
+        q.push(u);
       }
     }
-    ++dist;
   }
-  sort(begin(d), end(d), [](auto &l, auto &r) { return l.se < r.se; });
-  int i;
-  for (i = 1; i < n; ++i) {
-    if (d[i].fi == x) {
-      break;
+  sort(begin(d), end(d), [](auto &l, auto &r) { return l.se > r.se; });
+  int ones = 0;
+  int len = n - 2;
+  for (int i = 0; i < len; ++i) {
+    if (d[i].se == 1) {
+      ++ones;
     }
   }
-  return i % 2 == 0;
+  int steps = len - ones;
+  if (steps % 2 == 0) {
+    return ((ones % 2 == 1) ? 1 : 2);
+  } else {
+    return ((ones % 2 == 0) ? 1 : 2);
+  }
 }
 
-
-void show_tree(G &g, int n) {
+void show_tree(const G &g, int n) {
   for (int i = 1; i < n; ++i) {
-    for (auto &li : g[i]) {
-      /*for (int e : li) {
-        printf("%d -> %d", i, e);
-      }*/      
+    for (const auto &e : g[i]) {
+      printf("%d -> %d", i, e);
     }
   }
 }
@@ -56,15 +61,15 @@ int main() {
   while (t--) {
     int n, x;
     cin >> n >> x;
+    ++n;
     G g(n);
-    for (int i = 0; i < n - 1; ++i) {
+    for (int i = 1; i < n - 1; ++i) {
       int u, v;
       cin >> u >> v;
-      g[u].push_back(v); 
+      g[u].push_back(v);
       g[v].push_back(u);
     }
-    show_tree(g, n);
-    cout << (solve(g, n, x) ? "Ayush" : "Ashish") << "\n";
+    cout << (solve(g, n, x) == 1 ? "Ayush" : "Ashish") << "\n";
   }
   cout << endl;
   return 0;
