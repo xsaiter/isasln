d8f679198d3d651ccbd247e8ca9262fd1ab99a31
xsaiter <xsaiter@mail.ru>
2020-06-10 12:40:15 +0300

    improve

--- src/olymp/codef/contests/1360/g/sol.cpp
+++ src/olymp/codef/contests/1360/g/sol.cpp
@@ -2,21 +2,14 @@
 
 using namespace std;
 
-int solve(int n, int k) {
-  int res = 0;
-    
-  return res;
-}
-
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int t;
-  cin >> t;   
+  cin >> t;
   while (t--) {
-    int n, k;
-    cin >> n >> k;
-    printf("%d\n", solve(n ,k));
-	}
+    int n, m, a, b;
+    cin >> n >> m >> a >> b;
+  }
   return 0;
 }
---
+++ src/olymp/codef/contests/1362/a/sol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    ll a, b;
+    cin >> a >> b;
+  }
+  return 0;
+}
