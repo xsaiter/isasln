816cb66cdb634353bd5ba955fa7e4b593d1f24d3
xsaiter <xsaiter@mail.ru>
2020-06-13 15:32:01 +0300

    cf start 1271 c

---
+++ src/olymp/codef/contests/1271/c/sol.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n, sx, sy;
+  cin >> n >> sx >> sy;
+  vector<int> x(n), y(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> x[i] >> y[i];
+  }
+  return 0;
+}
