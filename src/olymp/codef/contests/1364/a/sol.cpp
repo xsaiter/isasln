cec596bf2e07a534a2e81f52046cf2165ef76a34
xsaiter <xsaiter@mail.ru>
2020-06-15 23:56:49 +0300

    improve

--- src/olymp/codef/contests/1364/a/sol.cpp
+++ src/olymp/codef/contests/1364/a/sol.cpp
@@ -11,6 +11,8 @@ int main() {
     vector<int> a(n);
     for (int i = 0; i < n; ++i) {
       cin >> a[i];
+    }    
+    for (int i = 0; i < n; ++i) {
     }
   }
   return 0;
---
+++ src/olymp/codef/contests/794/b/sol.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n, h;
+  cin >> n >> h;  
+  vector<double> x(n - 1);
+
+  return 0;
+}
