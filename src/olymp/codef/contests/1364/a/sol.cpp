754247c3e0345d94f51312120a8d081f30efab39
xsaiter <xsaiter@mail.ru>
2020-06-17 00:02:22 +0300

    cf 1364 a

--- src/olymp/codef/contests/1364/a/sol.cpp
+++ src/olymp/codef/contests/1364/a/sol.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   int t;
   cin >> t;
   while (t--) {
@@ -11,9 +13,39 @@ int main() {
     vector<int> a(n);
     for (int i = 0; i < n; ++i) {
       cin >> a[i];
-    }    
-    for (int i = 0; i < n; ++i) {
     }
+    vector<int> b(n);
+    int s = 0;
+    for (int i = 0; i < n; ++i) {
+      s += a[i];
+      b[i] = ((s % x) == 0 ? 0 : 1);
+    }
+    int len = 0;
+    bool ok = false;
+    for (int i = n - 1; i >= 0; --i) {
+      if (b[i] == 1) {
+        len = i + 1;
+        ok = true;
+        break;
+      }
+    }
+    for (int i = 0; i < n; ++i) {
+      if (b[i] == 0) {
+        if (len - 1 < i) {
+          for (int j = 0; j < i; ++j) {
+            if (b[j] == 1) {
+              if (i - j > len) {
+                len = i - j;
+                ok = true;
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+    cout << (ok ? len : -1) << "\n";
   }
+  cout << endl;
   return 0;
 }
