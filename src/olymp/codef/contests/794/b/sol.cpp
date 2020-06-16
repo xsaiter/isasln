4a63865f416cb974ea24dc5b63f7bd81ac14f8bf
xsaiter <xsaiter@mail.ru>
2020-06-17 01:23:36 +0300

    cf 794 b

--- src/olymp/codef/contests/794/b/sol.cpp
+++ src/olymp/codef/contests/794/b/sol.cpp
@@ -1,19 +1,48 @@
 #include <bits/stdc++.h>
 
+#define fi first
+#define se second
+
 using namespace std;
 
-dobule area_trap(double a, double b, double h) {
+using pdd = pair<double, double>;
+
+double area_trap(double a, double b, double h) {
   return 0.5 * h * (a + b);
 }
 
-
+pdd calc_ht(double a, double s, double z) {
+  double d = 4 * a * a * z * z - 4 * a * 2 * s * z;
+  double sd = sqrt(d);
+  return {(2 * a * z + sd) / (2 * a), (2 * a * z - sd) / (2 * a)};
+}
 
 int main() {
   int n, h;
-  cin >> n >> h;  
-  vector<double> x(n);
+  cin >> n >> h;    
   double ss = 0.5 * h * 1;
   double s = ss / n;
-
+  int m = n - 1;
+  vector<double> a(m);
+  a[0] = 1;
+  vector<double> hh(m, 0);
+  for (int i = 0; i < m; ++i) {    
+    double x = 0;
+    for (int j = 0; j < i; ++j) {
+      x += hh[j];
+    }
+    double z = h - x;   
+    hh[i] = calc_ht(a[i], s, z).se;    
+  }
+  vector<double> res(m);
+  double cur = 0;
+  for (int i = 0; i < m; ++i) {
+    cur = hh[i];
+    res[i] = cur;
+  }
+  for (int i = 0; i < m; ++i) {
+    printf("%.12lf ", res[i]);
+  }
+  cout << endl;
   return 0;
 }
