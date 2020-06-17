e9e7469ec6a58bee88c4088c1be396797c0e2b58
xsaiter <xsaiter@mail.ru>
2020-06-17 10:30:36 +0300

    improve cf 1364 a

--- src/olymp/codef/contests/1364/a/sol.cpp
+++ src/olymp/codef/contests/1364/a/sol.cpp
@@ -10,41 +10,26 @@ int main() {
   while (t--) {
     int n, x;
     cin >> n >> x;
-    vector<int> a(n);
+    int c, len = 0, s = 0, p = -1;
     for (int i = 0; i < n; ++i) {
-      cin >> a[i];
-    }
-    vector<int> b(n);
-    int s = 0;
-    for (int i = 0; i < n; ++i) {
-      s += a[i];
-      b[i] = ((s % x) == 0 ? 0 : 1);
-    }
-    int len = 0;
-    bool ok = false;
-    for (int i = n - 1; i >= 0; --i) {
-      if (b[i] == 1) {
-        len = i + 1;
-        ok = true;
-        break;
-      }
-    }
-    for (int i = 0; i < n; ++i) {
-      if (b[i] == 0) {
-        if (len - 1 < i) {
-          for (int j = 0; j < i; ++j) {
-            if (b[j] == 1) {
-              if (i - j > len) {
-                len = i - j;
-                ok = true;
-                break;
-              }
-            }
+      cin >> c;
+      s += c;
+      if (s % x > 0) {
+        if (p == -1) {
+          p = i;
+          len = i + 1;
+        } else if (i + 1 > len) {
+          len = i + 1;
+        }
+      } else {
+        if (p != -1) {
+          if (len < i - p) {
+            len = i - p;
           }
         }
       }
-    }
-    cout << (ok ? len : -1) << "\n";
+    }    
+    cout << (p != -1 ? len : -1) << "\n";
   }
   cout << endl;
   return 0;
