fec4027fc4688e9c6a359f82a6357652e4ee5635
xsaiter <xsaiter@mail.ru>
2020-06-23 00:35:11 +0300

    cf 245 b

--- src/olymp/codef/contests/245/b/sol.cpp
+++ src/olymp/codef/contests/245/b/sol.cpp
@@ -2,18 +2,36 @@
 
 using namespace std;
 
-string solve(string &s) {
-  string res;
-  int n = (int) s.size();
-
-  return res;
-}
+string s;
+ostringstream os;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  string s;
   cin >> s;
-  cout << solve(s) << endl;
+  int n = (int)s.size();
+  int i;
+  if (s[0] == 'f') {
+    os << "ftp"; i = 3;
+  } else {
+    os << "http"; i = 4;
+  }
+  os << "://";
+  int j = n - 1;
+  while (true) {    
+    if (s[j] == 'u' && s[j - 1] == 'r') break;
+    --j;
+  }
+  for (int k = i; k < j - 1; ++ k) {
+    os << s[k];
+  }
+  os << ".ru";
+  if (j != n - 1) {
+    os << "/";
+    for (int k = j + 1; k < n; ++k) {
+      os << s[k];
+    }
+  }
+  cout << os.str() << endl;
   return 0;
 }
