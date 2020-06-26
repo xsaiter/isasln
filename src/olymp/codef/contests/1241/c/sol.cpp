bd1accd27c7633be6859eac7559c02be815c7b87
xsaiter <xsaiter@mail.ru>
2020-06-27 01:52:09 +0300

    cf

---
+++ src/olymp/codef/contests/1241/c/sol.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int q;
+  cin >> q;
+  while (q--) {
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for (int i = 0; i < n; ++i) {
+      cin >> p[i];
+    }
+    int x, a, y, b, k;
+    cin >> x >> a >> y >> b >> k;
+
+  }
+  return 0;
+}
--- src/olymp/codef/contests/141/b/sol.cpp
+++ src/olymp/codef/contests/141/b/sol.cpp
@@ -2,24 +2,47 @@
 
 using namespace std;
 
-struct P { int x, y; };
-struct R { P a, c; };
-
-bool inside(const R &r, const P &p) {
-  return r.a.x < p.x && r.a.y < p.y && r.c.x > p.x && r.c.y > p.y;
+bool inside(int a, int x) {
+  return (-a < x && x < a);
 }
 
-int solve(int a, int b, int c) {
-  int res = -1;
+bool inside2(int a, int x) {
+  return (((double)-a / 2) < x && x < ((double)a / 2));
+}
 
-  return res;
+int solve(int a, int x, int y) {
+  int t = y % a;
+  if (t == 0) return -1;
+  int h = (y - t) / a + 1;
+  if (h < 3) {    
+    if (inside2(a, x)) return h;
+  } else {
+    bool ins = (h % 2 == 0) ? inside2(a, x) : inside(a, x);    
+    if (ins) {
+      int p = h - 2;
+      int r;
+      if (p == 1) {
+        if (x == 0) return -1;
+        if (x < 0) r = 1; else r = 2;
+      } else if (p % 2 == 0) {
+        r = (p / 2) * 2 + (p / 2);
+      } else {
+        int q = (p - 1) / 2;        
+        r = q + (p - q) * 2;
+        if (x == 0) return -1;
+        if (x < 0) --r;
+      }
+      return 2 + r;
+    }
+  }
+  return -1;
 }
 
 int main() {
   ios::sync_with_stdio(false);
-  cit.tie(nullptr);
+  cin.tie(nullptr);
   int a, x, y;
   cin >> a >> x >> y;
-  cout << solve(a, b, c) << endl;
+  cout << solve(a, x, y) << endl;
   return 0;
 }
