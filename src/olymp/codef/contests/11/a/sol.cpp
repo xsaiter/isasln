62c647566cc4d55278ccd48d18197e74220d406f
xsaiter <xsaiter@mail.ru>
2020-07-05 23:30:24 +0300

    cf

---
+++ src/olymp/codef/contests/11/a/sol.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int n, d;
+  cin >> n >> d;
+  vector<int> b(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> b[i];
+  }
+  int res = 0;
+  for (int i = 0; i < n; ++i) {
+
+  }
+  cout << res << endl;
+  return 0;
+}
