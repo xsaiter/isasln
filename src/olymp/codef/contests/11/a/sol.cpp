65c02dcd6e07b6dfc2f94c88046e6df0f7ab2ac6
xsaiter <xsaiter@mail.ru>
2020-07-06 09:15:05 +0300

    cf

--- src/olymp/codef/contests/11/a/sol.cpp
+++ src/olymp/codef/contests/11/a/sol.cpp
@@ -3,15 +3,22 @@
 using namespace std;
 
 int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
   int n, d;
   cin >> n >> d;
-  vector<int> b(n);
-  for (int i = 0; i < n; ++i) {
-    cin >> b[i];
-  }
+  int p, c;
+  cin >> p;
   int res = 0;
-  for (int i = 0; i < n; ++i) {
-
+  for (int i = 1; i < n; ++i) {
+    cin >> c;
+    int q = p - c;
+    if (q >= 0) {      
+      int x = 1 + (q - (q % d)) / d;
+      res += x;
+      c += d * x;
+    }    
+    p = c;
   }
   cout << res << endl;
   return 0;
