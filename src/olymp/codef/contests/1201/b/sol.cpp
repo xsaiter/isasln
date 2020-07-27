4aab91c50f2f01e84b1cc41078a59e86a7af40e0
xsaiter <xsaiter@mail.ru>
2020-07-28 00:42:04 +0300

    cf

--- src/olymp/codef/contests/1201/b/sol.cpp
+++ src/olymp/codef/contests/1201/b/sol.cpp
@@ -1,9 +1,14 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using R = long long;
 
 bool solve(vector<int> &a, int n) {
-  return false;
+  R sum_all = accumulate(begin(a), end(a), 0LL);
+  if (sum_all % 2 != 0) return false;
+  R biggest = *(max_element(begin(a), end(a)));
+  R sum_other = sum_all - biggest;   
+  return biggest <= sum_other;
 }
 
 int main() {
@@ -14,7 +19,7 @@ int main() {
   vector<int> a(n);
   for (int i = 0; i < n; ++i) {
     cin >> a[i];
-  }
+  }  
   cout << (solve(a, n) ? "YES" : "NO") << endl;
   return 0;
 }
---
+++ src/olymp/codef/contests/1201/c/sol.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(vector<int> &a, int n) {
+  int res = 0;
+  sort(begin(a), end(a));
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }  
+  cout << solve(a, n) << endl;
+  return 0;
+}
