bc396bd7a31d47659cf11356e56dbb6f5b04b611
xsaiter <xsaiter@mail.ru>
2020-07-30 11:11:23 +0300

    cf

--- src/olymp/codef/contests/1382/b/sol.cpp
+++ src/olymp/codef/contests/1382/b/sol.cpp
@@ -2,6 +2,28 @@
 
 using namespace std;
 
+int solve(vector<int> &a, int n) { 
+  bool all_one = true;
+  for (int i = 0; i < n; ++i) {
+    if (a[i] != 1) {
+      all_one = false;
+      break;
+    }
+  }
+  if (all_one) {
+    if (n % 2 == 0) return 2; else return 1;
+  }
+  int j = 0;
+  for (int i = 0; i < n; ++i) {
+    if (a[i] != 1) {
+      break;
+    } else {
+      ++j;
+    }
+  }
+  if (j % 2 == 0) return 1; else return 2;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -14,7 +36,8 @@ int main() {
     for (int i = 0; i < n; ++i) {
       cin >> a[i];
     }
-
+    cout << (solve(a, n) == 1 ? "First" : "Second") << "\n";
   }
+  cout << endl;
   return 0;
 }
