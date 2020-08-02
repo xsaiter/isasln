eed3155ee676c34888ba3703fe038e99152eefc6
xsaiter <xsaiter@mail.ru>
2020-08-02 13:27:33 +0300

    cf

--- src/olymp/codef/contests/1200/a/sol.cpp
+++ src/olymp/codef/contests/1200/a/sol.cpp
@@ -9,7 +9,32 @@ int main() {
   cin >> n;
   string s;
   cin >> s;
+  int m = 10;
+  vector<int> res(m, 0);
   for (int i = 0; i < n; ++i) {
+    if (s[i] == 'L') {
+      int j = 0;
+      while (j <= m - 1) {
+        if (res[j] == 0) {
+          res[j] = 1;
+          break;
+        }
+        ++j;
+      }
+    } else if (s[i] == 'R') {
+      int j = m - 1;
+      while (j >= 0) {
+        if (res[j] == 0) {
+          res[j] = 1;
+          break;
+        }
+        --j;
+      }
+    } else {
+      int j = (int)(s[i] - '0');
+      res[j] = 0;
+    }
   }
+  copy(begin(res), end(res), ostream_iterator<int>(cout, ""));
   return 0;
 }
---
+++ src/olymp/codef/contests/841/c/sol.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int m;
+  vector<int> a(m), b(m);
+  for (int i = 0; i < m; ++i) {
+    cin >> a[i];
+  }
+  for (int i = 0; i < m; ++i) {
+    cin >> b[i];
+  }
+  return 0;
+}
