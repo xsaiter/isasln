0f53b74fffbe893e7aca9c62e77ba88562b81cb8
xsaiter <xsaiter@mail.ru>
2020-08-03 23:11:15 +0300

    cf

--- src/olymp/codef/contests/1300/b/sol.cpp
+++ src/olymp/codef/contests/1300/b/sol.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+int solve(vector<int> &a, int m) {
+  int res = 0;
+  return res;
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -10,10 +15,13 @@ int main() {
   while (t--) {
     int n;
     cin >> n;
-    vector<int> a(2*n);
-    for (int i = 0 ; i< 2*n; ++i) {
+    int m = 2 * n;
+    vector<int> a(m);
+    for (int i = 0; i < m; ++i) {
       cin >> a[i];
     }
+    cout << solve(a, m) << "\n";
   }
+  cout << endl;
   return 0;
 }
