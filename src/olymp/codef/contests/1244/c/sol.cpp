150d106c549a5d6417de426ab73f49a50a2c52e6
xsaiter <xsaiter@mail.ru>
2020-08-31 09:01:22 +0300

    cf

---
+++ src/olymp/codef/contests/1244/c/sol.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, p, w, d;
+  cin >> n >> p >> w >> d;
+  vector<int> v;
+  string s;  
+  // x * w + y * d = p
+  // x + y + z = n;
+
+  return 0;
+}
