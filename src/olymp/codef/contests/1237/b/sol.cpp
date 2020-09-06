2d354ac5e142f1b96d06459239b08582ac23e8c8
xsaiter <xsaiter@mail.ru>
2020-09-06 17:52:32 +0300

    cf

--- src/olymp/codef/contests/1237/b/sol.cpp
+++ src/olymp/codef/contests/1237/b/sol.cpp
@@ -1,9 +1,37 @@
 #include <bits/stdc++.h>
 
+#define first fi
+#define second se
+
 using namespace std;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  vector<int> b(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> b[i];
+  }    
+  vector<int> x(n);
+  for (int i = n - 1; i >= 0; --i) {        
+    x[b[i]] = n - 1 - i;
+  }
+  int res = 0;
+  for (int i = n - 1; i >= 0; --i) {    
+    auto pos = x[a[i]];    
+    for (int j = i - 1; j >= 0; --j) {
+      if (x[a[j]] < pos) {
+        ++res;
+        break;
+      }
+    }
+  }
+  cout << res << endl;
   return 0;
 }
