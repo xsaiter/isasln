2e583aab50f81294e574b23a7196a53e5bc13593
xsaiter <xsaiter@mail.ru>
2020-09-19 13:30:37 +0300

    cf

--- src/olymp/codef/contests/1400/b/sol.cpp
+++ src/olymp/codef/contests/1400/b/sol.cpp
@@ -6,16 +6,13 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int t;
-  cin >> t;
   while (t--) {
-    int n;
-    cin >> n;    
-    string s;
-    cin >> s;
-    string res(n, s[n - 1]);  
-    cout << res << "\n";
+    int p, f;
+    cin >> p >> f;
+    int cs, cw;
+    cin >> cs >> cw;
+    int s, w;
+    cin >> s >> w;
   }
-  cout << endl;
   return 0;
 }
-
