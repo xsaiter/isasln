e1873bfecf6de78668f4e0eb5796e405e259a068
xsaiter <xsaiter@mail.ru>
2020-10-17 20:17:43 +0300

    1399 b

--- src/olymp/codef/contests/1399/b/sol.cpp
+++ src/olymp/codef/contests/1399/b/sol.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 
-int solve(vector<int> &a, vector<int> &b, int n) {
-  int res = 0;
+using i64 = long long;
+
+i64 solve(vector<int> &a, vector<int> &b, int n) {
+  i64 res = 0;
+  int mi_a = *min_element(begin(a), end(a));
+  int mi_b = *min_element(begin(b), end(b));
+  for (int i = 0; i < n; ++i) {
+    res += (i64)max(a[i] - mi_a, b[i] - mi_b);
+  }
   return res;
 }
 
 int main() {
-  ios::sync+_with_stdio(false);
+  ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int t;
   cin >> t;
