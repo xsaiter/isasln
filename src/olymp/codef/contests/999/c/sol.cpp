c979adef118414270bd278ca14d556e3a0d56f0e
xsaiter <xsaiter@mail.ru>
2020-11-06 00:10:47 +0300

    cf

--- src/olymp/codef/contests/999/c/sol.cpp
+++ src/olymp/codef/contests/999/c/sol.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+void solve(string &s, int n, int k) {
+  map<char, vector<int>> m;
+  for (int i = 0; i < n; ++i) {
+    auto it = m.find(s[i]);
+    if (it != s.end()) {
+      
+    }
+  }
+  int a = (int)'a';
+  int z = (int)'z';
+  
+}
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
@@ -9,6 +22,7 @@ int main() {
   cin >> n >> k;
   string s;
   cin >> s;
-
+  solve(s, n, k);
+  cout << s << endl;
   return 0;
 }
