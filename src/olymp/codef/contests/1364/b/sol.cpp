3935de5c5f63ac945df317bc662376f4b1c45c28
xsaiter <xsaiter@mail.ru>
2020-12-21 12:47:17 +0300

    improve

---
+++ src/olymp/codef/contests/1364/b/sol.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  sio::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  while (t--) {
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    for (int i = 0; i < n; ++i) {
+       cin >> p[n];
+    }
+
+  }
+  return 0;
+}
