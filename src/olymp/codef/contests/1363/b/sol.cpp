dff6603b59d8e363db54ca507b2e32fabf236d32
xsaiter <xsaiter@mail.ru>
2021-01-03 22:27:54 +0300

    1363 b

--- src/olymp/codef/contests/1363/b/sol.cpp
+++ src/olymp/codef/contests/1363/b/sol.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
-int solve(string &s) {
-  int res = 0;
+int solve(const string &s) {  
+  int n = (int)s.size();
+  int k0 = 0, k1 = 0;
+  for (int i = 0; i < n; ++i) {
+    if (s[i] == '0') ++k0; else ++k1;
+  }
+  int res = min(k0, k1);
+  int p0 = 0, p1 = 0;
+  for (int i = 0; i < n; ++i) {
+    if (s[i] == '0') { 
+      ++p0;
+      --k0;
+    } else {
+      ++p1;
+      --k1;
+    }
+    res = min(res, p0 + k1);
+    res = min(res, p1 + k0);
+  }
   return res;
 }
 
@@ -15,6 +32,6 @@ int main() {
     cin >> s;
     cout << solve(s) << "\n";
   }
-  cou << endl;
+  cout << endl;
   return 0;
 }
