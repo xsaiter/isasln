c6e443b0955927fb3e5c3ea9a4285c276081812f
xsaiter <xsaiter@mail.ru>
2021-03-01 19:21:57 +0300

    improve

---
+++ src/olymp/codef/contests/1354/c1/sol.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using R = double;
+
+R solve(int n) {
+  R res = 0;
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    cout << solve(n) << "\n";
+  }
+  cout << endl;
+  return 0;
+}
