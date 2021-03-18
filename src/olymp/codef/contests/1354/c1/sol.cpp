c28e01347518382cada2c4b32c225697d61ea354
xsaiter <xsaiter@mail.ru>
2021-03-18 12:51:34 +0300

    1354 c1

--- src/olymp/codef/contests/1354/c1/sol.cpp
+++ src/olymp/codef/contests/1354/c1/sol.cpp
@@ -1,11 +1,22 @@
 #include <bits/stdc++.h>
 
+#define PI 3.14159265358979323846
+
 using namespace std;
 
-using R = double;
-
-R solve(int n) {
-  R res = 0;
+double solve(int n) {
+  if (n == 2) return 1;  
+  int m = 2 * n;
+  int k = (m - 4) / 4;
+  double fi = ((m - 2) * PI) / m;
+  double a = fi - PI / 2;
+  double res = cos(a);
+  for (int i = 0; i < k - 1; ++i) {
+    a = fi - (PI / 2 - a) - PI / 2;
+    res += cos(a);
+  }
+  res *= 2;
+  res += 1;
   return res;
 }
 
@@ -17,7 +28,7 @@ int main() {
   while (t--) {
     int n;
     cin >> n;
-    cout << solve(n) << "\n";
+    cout << setprecision(9) << fixed << solve(n) << "\n";
   }
   cout << endl;
   return 0;
---
+++ src/olymp/codef/contests/1355/a/sol.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using X = long long;
+using P = pair<int, int>;
+
+P get_digits(X n) {
+  
+}
+
+X solve(X a1, X k) {
+  X res = 0;
+  return res;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    X a1, k;
+    cin >> a1 >> k;
+    cout << solve(a1, k) << "\n";
+  }
+  cout << endl;
+  return 0;
+}
