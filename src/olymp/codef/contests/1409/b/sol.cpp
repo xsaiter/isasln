1603944ada945b267e3581fa5104b827cc2e4266
xsaiter <xsaiter@mail.ru>
2021-07-10 21:50:46 +0300

    1409 b

--- src/olymp/codef/contests/1409/b/sol.cpp
+++ src/olymp/codef/contests/1409/b/sol.cpp
@@ -3,13 +3,32 @@
 using namespace std;
 using R = long long;
 
-R solve(int a, int b, int x, int y, int n) {  
+inline R mul(int x, int y) {
+  return (R)x * (R)y;
+}
+
+R get(int a, int b, int x, int y, int n) {
   R res = 0;
-  int aa = a - x;
-  int bb = b - y;
-  if (a == b) {    
-  } 
-  return res;
+  int d = a - x;
+  if (d >= n) {
+    a -= n;
+    res = mul(a, b);
+  } else {
+    n -= d;
+    d = b - y;
+    if (d >= n) {
+      res = mul(x, b - n);
+    } else {
+      res = mul(x, y);
+    }
+  }
+  return res;  
+}
+
+R solve(int a, int b, int x, int y, int n) {    
+  R sa = get(a, b, x, y, n);
+  R sb = get(b, a, y, x, n);
+  return min(sa, sb);
 }
 
 int main() {
