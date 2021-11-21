c0919741afed898b44504bcd8a4c882faf517cfb
xsaiter <xsaiter@mail.ru>
2021-11-21 22:32:19 +0300

    1506 b begin

---
+++ src/olymp/codef/contests/1506/b/sol.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(int n, int k, string &s) {
+  int res = 0;
+  return res;
+}
+
+int main() {
+  int tt;
+  cin >> tt;
+  while (tt--) {        
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+  }
+  return 0;
+}
