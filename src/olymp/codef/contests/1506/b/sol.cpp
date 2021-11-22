19173e9168c3fab144731aaf74b565cc43b06213
xsaiter <xsaiter@mail.ru>
2021-11-22 23:17:56 +0300

    1506 b complete

--- src/olymp/codef/contests/1506/b/sol.cpp
+++ src/olymp/codef/contests/1506/b/sol.cpp
@@ -2,19 +2,42 @@
 
 using namespace std;
 
-int solve(int n, int k, string &s) {
+int find_next_pos(string &s, int n, int k, int start) {
+  int j = start + 1;
+  int res = j;
+  while (j < n && j - start <= k) {
+    if (s[j] == '*') {
+      res = j;
+    }
+    ++j;
+  }
+  return res;
+}
+
+int solve(string &s, int n, int k) {    
   int res = 0;
+  int i = 0; 
+  while (i < n) {
+    if (s[i] == '*') {
+      ++res;
+      i = find_next_pos(s, n, k, i);
+    } else {
+      ++i;
+    }
+  }
   return res;
 }
 
 int main() {
-  int tt;
-  cin >> tt;
-  while (tt--) {        
+  int t;
+  cin >> t;
+  while (t--) {        
     int n, k;
     cin >> n >> k;
     string s;
     cin >> s;
+    cout << solve(s, n, k) << "\n";
   }
+  cout << endl;
   return 0;
 }
