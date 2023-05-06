c6495e7be9baad58111461ec7318adff0e965808
xsaiter <xsaiter@mail.ru>
2023-05-06 16:39:03 +0300

    1701

---
+++ src/olymp/codef/contests/1701/a/sol.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;  
+  vector<int> a;
+  sort(a.begin(), a.end());
+  while (t--) {  		 		              
+  
+  }
+  return 0;
+}
