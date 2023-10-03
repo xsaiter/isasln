bee734b11d40f0c7625ae7a1f734ccae55bf8940
xsaiter <xsaiter@mail.ru>
2023-10-03 07:41:22 +0300

    576

--- src/olymp/codef/contests/567/a/sol.cpp
+++ src/olymp/codef/contests/567/a/sol.cpp
@@ -12,24 +12,34 @@ int main() {
   vector<int> d(n);
   d[0] = 0;
   for (int i = 1; i < n; ++i) {    
-    d[i] = d[i - 1] + abs(abs(x[i]) - abs(x[i - 1]));
+    int len = 0;
+    if (x[i - 1] < 0) {
+      if (x[i] < 0) {
+        len = abs(x[i - 1]) - abs(x[i]);
+      } else {
+        len = abs(x[i - 1]) + x[i];
+      }
+    } else {
+      len = x[i] - x[i - 1];
+    }
+    d[i] = d[i - 1] + len;
   }
   for (int i = 0; i < n; ++i) {
     int mi = -1;
     if (i == 0) {
       mi = d[1];
     } else if (i == n - 1) {
-      mi = d[n - 1];
+      mi = d[n - 1] - d[n - 2];
     } else {
-      mi = min(d[i + 1], d[i - 1]);
+      mi = min(d[i + 1] - d[i], d[i] - d[i - 1]);
     }    
     int ma = -1;
     if (i == 0) {
       ma = d[n - 1];
     } else if (i == n - 1) {
-      ma = d[0];
+      ma = d[n -1];
     } else {
-      ma = max(d[0], d[n - 1]);
+      ma = max(d[i] - d[0], d[n - 1] - d[i]);
     }
     cout << mi << " " << ma << '\n';
   }
