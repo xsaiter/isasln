ef5e6996d73664d81737563d7c279af0c6dc3aca
xsaiter <xsaiter@mail.ru>
2024-04-04 11:50:17 +0300

    1701 a etc

--- src/olymp/codef/contests/1701/a/sol.cpp
+++ src/olymp/codef/contests/1701/a/sol.cpp
@@ -2,15 +2,25 @@
 
 using namespace std;
 
+int solve(vector<int> &a) {  
+  if (a[0] == 0 && a[1] == 0 && a[2] == 0 && a[3] == 0) {
+    return 0;
+  }
+  if (a[0] == 1 && a[1] == 1 && a[2] == 1 && a[3] == 1) {
+    return 2;
+  }
+  return 1;
+}
+
 int main() {
   int t;
-  cin >> t;  
-  vector<int> a;
-  if (3 < 5) {
-  }
-  sort(a.begin(), a.end());      
-  
+  cin >> t;    
   while (t--) {  		 		                             
+    vector<int> a(4);
+    cin >> a[0] >> a[1];
+    cin >> a[2] >> a[3];
+    cout << solve(a) << '\n';
   }
+  cout << endl;
   return 0;
 }
---
+++ src/olymp/codef/contests/1708/b/sol.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, l, r;
+    cin >> n >> l >> r;
+  }
+  return 0;
+}
