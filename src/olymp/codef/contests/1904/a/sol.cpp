3f7cc735e7e2801df6282ad31f4f8dc3dc255df4
xsaiter <xsaiter@mail.ru>
2024-11-28 07:46:08 +0300

    1904 a

--- src/olymp/codef/contests/1904/a/sol.cpp
+++ src/olymp/codef/contests/1904/a/sol.cpp
@@ -12,6 +12,11 @@ int main() {
     cin >> xk >> yk;
     int xq, yq;
     cin >> xq >> yq;
+    int res = 0;
+    int dx = abs(xk - xq) - 1;
+    int dy = abs(yk - yq) - 1;
+    
+    cout << res << '\n';
   }
   cout << endl;
   return 0;
