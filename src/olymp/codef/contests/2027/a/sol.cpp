93a9187346ee2e72c3e102e76e38f5c2250a47b2
xsaiter <xsaiter@mail.ru>
2025-02-21 12:21:29 +0300

    2027 a

--- src/olymp/codef/contests/2027/a/sol.cpp
+++ src/olymp/codef/contests/2027/a/sol.cpp
@@ -6,18 +6,29 @@ struct Rect {
   int w, h;
 };
 
-int solve(int n, vector<Rect> &rr) {  
-  sort(begin(rr), end(rr), [](const Rect &a, const Rect &b) {
+int solve(int n, vector<Rect> &r) {  
+  sort(begin(r), end(r), [](const Rect &a, const Rect &b) {
     if (a.w == b.w) {
       return a.h < b.h;
     }
-    return a.w < b.w;
+    return a.w > b.w;
   });
 
-  int res = 0;
-  for (int i = 0; i < n; ++i) {
-
+  int res = 0;  
+  if (n == 1) {
+    return 2 * (r[0].w + r[0].h);
   }
+  int W = r[0].w;
+  int H = r[0].h;
+  res += 2 * W;  
+  res += 2 * H;    
+  for (int i = 1; i < n; ++i) {        
+    int dh = r[i].h - H;    
+    if (dh > 0) {
+      res += 2 * dh;            
+      H = r[i].h;
+    }    
+  }  
   return res;
 }
 
@@ -27,11 +38,11 @@ int main() {
   while (t--) {
     int n;
     cin >> n;
-    vector<Rect> rr(n);
+    vector<Rect> r(n);
     for (int i = 0; i < n; ++i) {
-      cin >> rr[i].w >> rr[i].h;
-    }
-    cout << solve(n, rr) << '\n';
+      cin >> r[i].w >> r[i].h;
+    }    
+    cout << solve(n, r) << '\n';    
   }
   cout << endl;
   return 0;
