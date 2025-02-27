51cb8d168e18d1a2f6c4fc3d84771fbc4b451a13
xsaiter <xsaiter@mail.ru>
2025-02-27 17:55:45 +0300

    2027 edit

--- src/olymp/codef/contests/2027/c/sol.cpp
+++ src/olymp/codef/contests/2027/c/sol.cpp
@@ -3,11 +3,22 @@
 using namespace std;
 
 // 0 < i <= len(a) and a[i] = len(a) + 1 - i
-// add (i - 1) 0 at the ending of array
+// adding (i - 1) of 0 at the ending of array
+
+int dp(int n, int len, vector<int> &a, vector<int> &idx) {
+  int res = n;
+  for (int i = 0; i < n; ++i) {
+    if (a[i] == len + 1 - i) {      
+      int add = i - 1;
+      res = max(res + add, dp(n, len + add, a));
+    }
+  }
+  return res;
+}
 
 int solve(int n, vector<int> &a) {
-  int res = 0;
-  return res;
+  vector<int> idx(n);
+  return dp(n, n, a);
 }
 
 int main() {
