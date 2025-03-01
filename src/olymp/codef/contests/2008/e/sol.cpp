1864a22bc408ad12c1e99aef1b1434db52e039ab
xsaiter <xsaiter@mail.ru>
2025-03-02 00:02:31 +0300

    2008 e edit

--- src/olymp/codef/contests/2008/e/sol.cpp
+++ src/olymp/codef/contests/2008/e/sol.cpp
@@ -2,18 +2,51 @@
 
 using namespace std;
 
-int dp(int n, int k, string &s) {
+int calc(const vector<int> &a) {
   int res = 0;
+  int mx = 0;
+  int n = (int)a.size();
   for (int i = 0; i < n; ++i) {
+    res += a[i];
+    mx = max(mx, a[i]);
   }
+  res -= mx;
   return res;
 }
 
-int solve(int n, string &s) {
-  return dp(n, n, s);
+int answer(int n, const string &s, int skip) {  
+  vector<int> va(256, 0);
+  vector<int> vb(256, 0);
+  for (int i = 0; i < n; ++i) {
+    if (i == skip) {
+      continue;
+    }
+    int c = (int)s[i];
+    int j = i;
+    if (i > skip) {
+      --j;
+    }    
+    if (j % 2 == 0) {
+      va[c]++;
+    } else {
+      vb[c]++;
+    }
+  }  
+  return calc(va) + calc(vb);    
 }
 
-int main() {
+int solve(int n, const string &s) {
+  if (n % 2 == 0) {
+    return answer(n, s, -1);    
+  }
+  int res = INT_MAX;
+  for (int i = 0; i < n; ++i) {
+    res = min(res, answer(n, s, i));
+  }  
+  return res + 1;
+}
+
+int main() {  
   int t;
   cin >> t;
   while (t--) {
