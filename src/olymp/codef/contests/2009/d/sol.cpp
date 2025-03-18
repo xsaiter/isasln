5cc224a9918b925732dbdf547865bf7bb23e3d03
xsaiter <xsaiter@mail.ru>
2025-03-18 16:47:49 +0300

    2009 d init

--- src/olymp/codef/contests/2009/d/sol.cpp
+++ src/olymp/codef/contests/2009/d/sol.cpp
@@ -8,6 +8,23 @@ struct P {
 
 int solve(int n, vector<P> &a) {
   int res = 0;
+  vector<int> b, c;
+  for (int i = 0; i < n; ++i) {
+    if (a[i].y == 0) {
+      b.push_back(a[i].x);
+    } else {
+      c.push_back(a[i].x);
+    }
+  }
+  sort(begin(b), end(b));
+  sort(begin(c), end(c)); 
+    
+  int nb = (int)b.size();
+  int nc = (int)c.size();
+  int p = 0, q = 0;
+  while (p < nb && q < nc) {    
+    ++p; ++q;
+  }
   return res;
 }
 
