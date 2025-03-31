7ec04c2e15c67cecc6fa4536968d9ed5cbeef445
xsaiter <xsaiter@mail.ru>
2025-03-31 12:06:12 +0300

    2000 d complete

--- src/olymp/codef/contests/2000/d/sol.cpp
+++ src/olymp/codef/contests/2000/d/sol.cpp
@@ -1,17 +1,45 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+using L = long long;
 
-int solve(int n, const vector<int> &a, const string &s) {
-  int res = 0;
-  int l = 0, r = 0;  
-  while (true) {
-       
+L sum_range(const vector<L> &d, int l, int r) {
+  if (l == 0L) {
+    return d[r];
+  }  
+  return d[r] - d[l - 1];
+}
+
+vector<L> diff(const vector<int> &a, int n) {
+  vector<L> d(n);
+  d[0] = a[0];
+  for (int i = 1; i < n; ++i) {
+    d[i] = d[i - 1] + (L)a[i];
+  }
+  return d;
+}
+
+L solve(int n, const vector<int> &a, const string &s) {    
+  vector<L> d = diff(a, n);
+  L res = 0L;
+  int l = 0, r = n - 1;
+  while (l <= n - 1 && r >= 0 && l < r) {
+    if (s[l] == 'L') {
+      while (l < r) {
+        if (s[r] == 'R') {
+          res += sum_range(d, l, r);
+          --r;
+          break;
+        }
+        --r;
+      }
+    }
+    ++l;
   }
   return res;
 }
 
-int main() {
+int main() {  
   int t;
   cin >> t;
   while (t--) {
