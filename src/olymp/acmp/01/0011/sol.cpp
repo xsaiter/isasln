cf2a363a2145a1517c3c7ceff7e9a33e225f8c89
xsaiter <xsaiter@mail.ru>
2025-04-24 09:51:26 +0300

    acmp...

--- src/olymp/acmp/01/0011/sol.cpp
+++ src/olymp/acmp/01/0011/sol.cpp
@@ -2,20 +2,19 @@
 
 using namespace std;
 
-using i64 = long long;
-using vi64 = vector<i64>;
+using L = long long;
 
 int main() {
-  i64 k, n;
+  L k, n;
   cin >> k >> n;
   ++n;
-  vi64 a(n, 0);
-  a[0] = 1;
-  for (i64 i = 1; i < n; ++i) {
-    for (i64 j = 1; j <= min(k, i); ++j) {
-      a[i] += a[i - j];
+  vector<L> dp(n, 0);
+  dp[0] = 1;
+  for (L i = 1; i < n; ++i) {
+    for (L j = 1; j <= min(k, i); ++j) {
+      dp[i] = dp[i] + dp[i - j];
     }
   }
-  cout << a[n - 1] << endl;
+  cout << dp[n - 1] << endl;
   return 0;
 }
