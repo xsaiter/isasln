f6bd8a4e8553ae75fd7562022a5ed8055db3ed9c
xsaiter <xsaiter@mail.ru>
2025-04-29 20:45:42 +0300

    acmp

--- src/olymp/acmp/07/0329/sol.cpp
+++ src/olymp/acmp/07/0329/sol.cpp
@@ -3,14 +3,28 @@
 using namespace std;
 
 struct R {
-  int sum;
-  int na;
-  vector<int> a;
+  int sum;  
+  vector<int> b;
 };
 
-R solve(int n, vector<int> &a) {
-  R r;
-  return r;
+R solve(int n, const vector<int> &a) {
+  vector<int> dp(n + 1);    
+  dp[0] = 0;
+  dp[1] = a[0];
+  for (int i = 1; i < n; ++i) {    
+    dp[i + 1] = max(dp[i], dp[i - 1]) + a[i];    
+  }
+  R res;
+  res.sum = dp[n];
+  for (int i = n; i >= 1; --i) {
+    int d = dp[i] - a[i - 1];
+    if (d == dp[i - 1]) {
+      res.b.push_back(i);
+    } else {
+      res.b.push_back(i - 1);
+    }
+  }
+  return res;
 }
 
 int main() {
@@ -20,6 +34,11 @@ int main() {
   for (int i = 0; i < n; ++i) {
     cin >> a[i];
   }
-  cout << solve(n, a) << endl;
+  R r = solve(n, a);
+  cout << r.sum << '\n';
+  int nb = (int)r.b.size();
+  for (int i = 0; i < nb; ++i) {
+    cout << r.b[i] << " ";
+  }  
   return 0;
 }
