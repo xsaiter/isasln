b04dfff55c1c3302dc31f2a092aa915841dfafc7
xsaiter <xsaiter@mail.ru>
2025-05-10 09:57:23 +0300

    acmp 46 done

--- src/olymp/acmp/01/0046/sol.cpp
+++ src/olymp/acmp/01/0046/sol.cpp
@@ -2,20 +2,31 @@
 
 using namespace std;
 
-const string e = "2.7182818284590452353602875";
+string e = "2.7182818284590452353602875";
+char digits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+int dig(char c) {
+  return (int)c - (int)'0';
+}
+              
+char from_dig(int x) {
+  return digits[x];
+}
 
 string solve(int n) {
   if (n == 0) {
     return "3";
-  }
-  const int len = (int)e.size();
-  int j = n + 2;
-  j = min(j, len - 1);
-  int c = (int)e[j];
-  if (j + 1 < len) {
-    int next = e[j + 1];
   }  
-  return e.substr(0, 0);
+  if (n == 25) {
+    return e;
+  }
+  int a = dig(e[n + 1]);
+  int b = dig(e[n + 1 + 1]);
+  if (b > 4) {
+    ++a;
+    e[n + 1] = from_dig(a);
+  }  
+  return e.substr(0, 2 + n);
 }
 
 int main() {
