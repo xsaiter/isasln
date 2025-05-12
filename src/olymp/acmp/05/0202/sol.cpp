9a14fca2e1808a1bc712f4fea79e4c152ee510d7
xsaiter <xsaiter@mail.ru>
2025-05-12 09:47:47 +0300

    acmp 202 done

--- src/olymp/acmp/05/0202/sol.cpp
+++ src/olymp/acmp/05/0202/sol.cpp
@@ -2,47 +2,50 @@
 
 using namespace std;
 
-vector<int> solve(const string &s, const string &t) {
-  vector<int> a;
-  int n = (int)s.size();
+vector<int> pref_f(const string& t) {
   int m = (int)t.size();
-  if (n < m) {
-    return a;
-  }
-  if (n == m) {
-    for (int i = 0; i < n; ++i) {
-      if (s[i] != t[i]) {
-        return a;
-      }
+  vector<int> a(m, 0);
+  int j = 0;
+  for (int i = 1; i < m; ++i) {
+    while (j > 0 && t[i] != t[j]) {
+      j = a[j - 1];
     }
-    a.push_back(0);
-    return a;
-  }  
-  for (int k = 0; k <= n - m; ++k) {
-    bool found = true;    
-    for (int j = 0; j < m; ++j) {
-      if (s[k + j] != t[j]) {          
-        found = false;
-        break;
-      } 
+    if (t[i] == t[j]) {
+      j += 1;
     }
-    if (found) {
-      a.push_back(k);
-    }
+    a[i] = j;
   }
   return a;
 }
 
+vector<int> solve(const string& s, const string& t) {
+  int n = (int)s.size();
+  int m = (int)t.size();
+  vector<int> pf = pref_f(t);
+  int j = 0;
+  vector<int> res;
+  for (int i = 0; i < n; ++i) {
+    while (j > 0 && s[i] != t[j]) {
+      j = pf[j - 1];
+    }
+    if (s[i] == t[j]) {
+      j += 1;
+    }
+    if (j == m) {
+      res.push_back(i - m + 1);
+      j = pf[j - 1];
+    }
+  }
+  return res;
+}
+
 int main() {
-  string s;
-  cin >> s;
-  string t;
-  cin >> t;
-  vector<int> a = solve(s, t);
-  int m = (int)a.size();
-  for (int i = 0; i < m; ++i) {
-    cout << a[i] << " ";
-  }  
+  string s, t;
+  cin >> s >> t;
+  vector<int> res = solve(s, t);
+  for (auto& v : res) {
+    cout << v << ' ';
+  }
   cout << endl;
   return 0;
 }
