d8e78e013b330a839651dd86805df1e67b403bb0
xsaiter <xsaiter@mail.ru>
2025-05-19 12:52:10 +0300

    acmp 706 done

---
+++ src/olymp/acmp/015/0706/sol.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+double solve(double r, double x, double y) {  
+  x = abs(x);
+  return r * x / (2 * r - y);    
+}
+
+int main() {
+  double r, x, y;
+  cin >> r >> x >> y;
+  double d = solve(r, x, y);  
+  printf("%.2f", d);  
+  return 0;
+}
