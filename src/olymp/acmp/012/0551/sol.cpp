b90de0c40893d6b140e1b6341219374f9dfc3c7a
xsaiter <xsaiter@mail.ru>
2025-06-03 17:40:58 +0300

    acmp 012 init

---
+++ src/olymp/acmp/012/0551/sol.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool solve(int R, int r, int h, int b) {
+  int d = 
+  return false;
+}
+
+int main() {
+  int R, r, h, b;
+  cin >> R >> r >> h >> b;
+  cout << (solve(R, r, h, b) ? "YES" : "NO") << endl;
+  return 0;
+}
