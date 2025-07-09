4923462dd9622500aeaa38e39cb599ee10b18c61
xsaiter <xsaiter@mail.ru>
2025-07-09 18:30:57 +0300

    acmp 89 done

--- src/olymp/acmp/02/0089/sol.cpp
+++ src/olymp/acmp/02/0089/sol.cpp
@@ -2,34 +2,97 @@
 
 using namespace std;
 
+const int D = 650;
+const int INF = 1e9;
+const int MM = 60;
+
 struct R {
   string name;
-  string a, b;
-  double speed;
+  int speed;
 };
 
-double speed_for(string a, string b) {
+int get_mins(int h, int m) {
+  return h * MM + m;
+}
+
+double to_hh(int mm) {
+  return (double)mm / (double)MM;
+}
+
+int get_speed(const string& a, const string& b) {
   int ha = atoi(a.substr(0, 2).c_str());
   int ma = atoi(a.substr(3, 2).c_str());
 
-  int hb = atoi(a.substr(0, 2).c_str());
-  int mb = atoi(a.substr(3, 2).c_str());  
+  int hb = atoi(b.substr(0, 2).c_str());
+  int mb = atoi(b.substr(3, 2).c_str());
+
+  int aa = get_mins(ha, ma);
+  int bb = get_mins(hb, mb);
+
+  int mm = bb - aa;
+  if (mm == 0) {
+    return -INF;
+  }
+  if (mm < 0) {
+    /*if (aa == 0) {
+      mm = bb;
+    } else {*/
+    mm = get_mins(24, 0) - aa + bb;
+    //}
+  }
+  double val = (double)D / (double)to_hh(mm);
+  return (int)round(val);
+}
+
+void parse(const string& s, R& r) {
+  int len = (int)s.size();
+  ostringstream os, oa, ob;
+  int state = 0;
+  for (int j = 0; j < len; ++j) {
+    if (state == 0) {
+      os << s[j];
+      if (s[j] == '"') {
+        state = 1;
+      }
+    } else if (state == 1) {
+      os << s[j];
+      if (s[j] == '"') {
+        state = 2;
+      }
+    } else if (state == 2) {
+      if (s[j] == ' ') {
+        state = 3;
+      }
+    } else if (state == 3) {
+      if (s[j] == ' ') {
+        state = 4;
+      } else {
+        oa << s[j];
+      }
+    } else if (state == 4) {
+      ob << s[j];
+    }
+  }
+  r.name = os.str();
+  string a = oa.str();
+  string b = ob.str();
+  r.speed = get_speed(a, b);
 }
 
 int main() {
+  string s;
+  getline(cin, s);
   int n;
-  cin >> n;
-  R best;  
-  best.speed = -1;
+  n = atoi(s.c_str());
+  vector<R> a(n);
   for (int i = 0; i < n; ++i) {
-    R r;
-    cin >> r.name >> r.a >> r.b;
-    r.speed = speed_for(r.a, r.b);
-    if (r.speed > best.speed) {
-      best = r;
-    }
+    getline(cin, s);
+    parse(s, a[i]);
   }
-  cout << "The fastest train is """ <<  best.name << """" << '\n';
-  cout << "Its speed is" << best.speed
+  sort(begin(a), end(a), [](const R& x, const R& y) {
+    return x.speed > y.speed;
+    });
+  cout << "The fastest train is """ << a[0].name << """." << '\n';
+  cout << "Its speed is " << a[0].speed << " km/h, approximately." << endl;
   return 0;
 }
--- src/olymp/acmp/02/0091/sol.cpp
+++ src/olymp/acmp/02/0091/sol.cpp
@@ -5,10 +5,13 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  int a = 2, b = 1;
-
+  int a, b;
+  vector<int> a(n + 1);
+  vector<int> b(n + 1);
+  a[1] = 2; a[2] = 3; a[4] = 
   for (int i = 1; i <= n; ++i) {
-    
+
   }
+  cout << a[n] << '\n' << b[n] << endl;
   return 0;
 }
