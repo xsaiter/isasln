482e16d61928beca2ccc1092a4f40be62278db9f
xsaiter <xsaiter@mail.ru>
2025-09-15 11:40:39 +0300

    acmp 948 done

--- src/olymp/acmp/019/0948/sol.cpp
+++ src/olymp/acmp/019/0948/sol.cpp
@@ -1,8 +1,27 @@
-#include <bits/*stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
-  int tl
+  int k, n;
+  cin >> k >> n;
+  int a, b;
+  if (k >= n) {
+    a = 1; b = n;
+  } else {
+    if (k == 1) {
+      a = n / k;
+      b = 1;
+    } else {
+      b = n % k;
+      if (b == 0) {
+        b = k;
+        a = n / k;
+      } else {
+        a = (n - b) / k + 1;
+      }
+    }
+  }
+  cout << a << ' ' << b << endl;
   return 0;
 }
