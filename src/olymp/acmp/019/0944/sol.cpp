6e525106dc811dab8095439e719e4d0491dba970
xsaiter <xsaiter@mail.ru>
2025-09-16 01:12:14 +0300

    acmp 944 done

---
+++ src/olymp/acmp/019/0944/sol.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int INF = 1e9;
+
+vector<int> load_dp(int n, const vector<int> &a, int s) {  
+  vector<int> dp(s + 1);
+  dp[0] = 0; 
+  for (int x = 1; x <= s; x++) {
+    dp[x] = INF;
+    for (int i = 0; i < n; ++i) {
+      int c = a[i];
+      int j = x - c;
+      if (j >= 0) {
+        dp[x] = min(dp[x], dp[j] + 1);
+      }
+    }
+  }
+  return dp;
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i];
+  }
+  int k;
+  cin >> k;
+  vector<int> b(k);
+  for (int i = 0; i < k; ++i) {
+    cin >> b[i];    
+  }
+  int mx = *max_element(begin(b), end(b));
+  vector<int> dp = load_dp(n, a, mx);
+  for (int i = 0; i < k; ++i) {
+    int ans = dp[b[i]];
+    if (ans == INF || ans <= 0) {
+      cout << 0;
+    } else {
+      cout << 1;
+    }
+    cout << ' ';
+  }
+  cout << endl;
+  return 0;
+}
