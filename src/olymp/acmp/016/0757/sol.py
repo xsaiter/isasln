1e621a972d66675ae6c1786ab27363bb130470ca
xsaiter <xsaiter@mail.ru>
2025-10-10 22:41:04 +0300

    improve 757

--- src/olymp/acmp/016/0757/sol.py
+++ src/olymp/acmp/016/0757/sol.py
@@ -1,15 +1,5 @@
 a, b, c = map(int, input().split())
 
+r = min(a // 2, b // 6, c)
 
-def solve():
-    if a < 2 or b < 6:
-        return 0
-
-    na = a // 2
-    nb = b // 6
-    nc = c
-
-    return min(na, nb, nc)
-
-
-print(solve())
+print(r)
