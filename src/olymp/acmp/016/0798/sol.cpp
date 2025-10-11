5f5e3f6ca1cfd1663b6d0c0337aed7307dd11809
xsaiter <xsaiter@mail.ru>
2025-10-11 17:23:41 +0300

    acmp 798 done

--- src/olymp/acmp/016/0798/sol.cpp
+++ src/olymp/acmp/016/0798/sol.cpp
@@ -2,20 +2,34 @@
 
 using namespace std;
 
+int solve(int m, int n, int i, int j, int c) {
+  if (m % 2 == 0 || n % 2 == 0) {
+    return 0;
+  }  
+  if (i > 1 && i % 2 == 0) {
+    if (c == 0) {
+      c = 1;
+    } else {
+      c = 0;
+    }
+  }
+  if (j > 1 && j % 2 == 0) {
+    if (c == 0) {
+      c = 1;
+    } else {
+      c = 0;
+    }
+  }
+  if (c == 0) {
+    return -1;
+  }
+  return 1;
+}
+
 int main() {
   int m, n, i, j, c;
   cin >> m >> n >> i >> j >> c;
-  int r = 0;
-  if (n != m) {
-    if (m % 2 == 0) {
-    } else {
-      if (i % 2 == 0) {
-        if (c == 0) {
-          r = 1;
-        }    
-      }
-    }    
-  }
+  int r = solve(m, n, i, j, c);  
   if (r == 0) {
     cout << "equal";
   } else if (r == 1) {
