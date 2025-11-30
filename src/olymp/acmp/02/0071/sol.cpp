4eb8be9cb8b4579047b712979829c2da8a43eca7
xsaiter <xsaiter@mail.ru>
2025-11-30 11:30:17 +0300

    acmp 986 done

--- src/olymp/acmp/02/0071/sol.cpp
+++ src/olymp/acmp/02/0071/sol.cpp
@@ -2,8 +2,25 @@
 
 using namespace std;
 
-int solve(int n, vector<int> &a) {
-  
+const int INF = 1e9;
+
+int sum(const vector<int> &a, int l, int r) {
+  int ans = 0;
+  for (int i = l; i <= r; ++i) {
+    ans += a[i];
+  }
+  return ans;
+}
+
+int solve(int n, const vector<int> &a, int l, int r) {
+  int best = INF;
+  sort(a.begin(), e.end());
+  for (int p = 1; p < n - 1; ++p) {    
+    for (int q = 0; q < n; ++q) {
+
+    }
+  }
+  return best;
 }
 
 int main() {
@@ -12,8 +29,8 @@ int main() {
   vector<int> a(n);
   for (int i = 0; i < n; ++i) {
     cin >> a[i];
-  }
-  int ans = solve(n, a);
-  cout << ans << endl;
+  }  
+  sort(a.begin(), a.end());
+  cout << solve(n, a, 0, n - 1) << endl;
   return 0;
 }
--- src/olymp/acmp/020/0986/sol.cpp
+++ src/olymp/acmp/020/0986/sol.cpp
@@ -2,6 +2,50 @@
 
 using namespace std;
 
+struct P {
+  int x, y;
+};
+
+struct R {
+  bool yes;
+  int k;
+};
+
+int dist2(const P &a, const P &b) {
+  return (a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y);
+}
+
+R solve(int n, const P &b, int l, vector<P> &a) {
+  R r;
+  r.yes = true;
+  int l2 = l * l;
+  for (int i = 0; i < n; ++i) {
+    if (l2 >= dist2(a[i], b)) {
+      r.yes = false;
+      r.k = i + 1;
+      break;
+    }
+  }
+  return r;
+}
+
 int main() {
+  int n;
+  cin >> n;
+  P b;
+  cin >> b.x >> b.y;
+  int l;
+  cin >> l;
+  vector<P> a(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> a[i].x >> a[i].y;
+  }
+  R r = solve(n, b, l, a);
+  if (r.yes) {
+    cout << "Yes";
+  } else {
+    cout << r.k;
+  }
+  cout << endl;
   return 0;
 }
