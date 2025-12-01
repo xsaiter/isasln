cbd3d1165f1bb39fb7110286bc41a6b96816e87d
xsaiter <xsaiter@mail.ru>
2025-12-01 11:23:01 +0300

    acmp 943 done

--- src/olymp/acmp/019/0943/sol.cpp
+++ src/olymp/acmp/019/0943/sol.cpp
@@ -3,9 +3,25 @@
 using namespace std;
 
 int solve(int n, int m, int y, int x) {
-  int r = 0;
-
-  return r;
+  int ans = 0;
+  for (int i = 1; i <= n; ++i) {
+    if (i % 2 == 1) {
+      for (int j = 1; j <= m; ++j) {
+        if (i == y && j == x) {
+          return ans;
+        }
+        ++ans;
+      }
+    } else {
+      for (int j = m; j >= 1; --j) {
+        if (i == y && j == x) {
+          return ans;
+        }
+        ++ans;
+      }
+    }    
+  }
+  return ans;
 }
 
 int main() {
