1139e1caf670973627578ce0efbd58dc66c009c1
xsaiter <xsaiter@mail.ru>
2025-12-06 23:26:18 +0300

    cf 5c init

--- src/olymp/cf/5/c/sol.cpp
+++ src/olymp/cf/5/c/sol.cpp
@@ -20,12 +20,43 @@ bool ok(const string &s, int p, int q) {
 int main() {
   string s;
   cin >> s;
-  int n = (int)s.size();
-  vector<vector<int>> dp(n + 1, vector<int>(n + 1));
-  for (int i = 0; i < n; ++i) {
-    for (int j = 0; j < n; ++j) {
+  int n = (int)s.size();  
+  int best_cnt = 0;
+  int best_len = -1;
+  int p = 0;
+  while (p < n) {
+    int z = 0;
+    int len = 0;    
+    int cur = 0;    
+    for (int i = p; i < n; ++i) {      
+      if (s[i] == '(') {
+        ++z;
+      } else {
+       --z;
+      }
+      if (z < 0) {
+        p = i;        
+        break;        
+      }
+      if (z == 0) {
+        ++cur;
+        len = cur;
+      }
+      if (z > 0) {
+        ++cur;
+      }
+    }    
+    if (len > best_len) {
+      best_len = len;
+      best_cnt = 1;
+    } else if (len == best_len) {
+      best_cnt++;
     }
+    ++p;
   }
-  cout << endl;
+  if (best_len == 0) {
+    best_cnt = 1;
+  }
+  cout << best_len << ' ' << best_cnt << endl;
   return 0;
 }
