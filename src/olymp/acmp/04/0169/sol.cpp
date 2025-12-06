3579f94a3c1d90dcb3c773ab3aca2bf5e10f7ef0
xsaiter <xsaiter@mail.ru>
2025-12-06 21:39:16 +0300

    acmp 169 done

--- src/olymp/acmp/04/0169/sol.cpp
+++ src/olymp/acmp/04/0169/sol.cpp
@@ -5,33 +5,23 @@ using namespace std;
 int main() {
   int n, k;
   cin >> n >> k;
-  vector<vector<int>> dp(n + 1, vector<int>(k + 1));  
-  dp[1][1] = 1;
-  for (int j = 2; j <= k; ++j) {
-    if (j % 2 == 1) {
-      dp[1][j] = dp[1][j] * 3;
-    } else {
-      dp[1][j] = dp[1][1];
-    }    
+  int m = n + k - 1;
+  vector<vector<int>> dp(m + 1, vector<int>(m + 1));
+  for (int i = 1; i <= m; ++i) {
+    dp[i][i] = 1;
   }
-  for (int i = 1; i <= n; ++i) {
-    for (int j = 1; j <= k; ++j) {
-      if (i == j) {
-        dp[i][j] = 1;
-      } else if (i > j) {
-        dp[i][j] = 0;
-      } else {
-        int d = j - i;
-        if (d == 1) {
-          dp[i][j] = dp[i][j - 1];
-        } else if (d % 2 == 0) { 
-          dp[i][j] = dp[i][j - 1] * 2;
-        } else {
-          dp[i][j] = dp[i][j - 1];
-        }
+  for (int j = 2; j <= m; ++j) {
+    for (int i = 1; i < j; ++i) {
+      int ii = i - 1, jj = j - 1;
+      if (ii >= 1 && jj >= 1) {
+        dp[i][j] += dp[ii][jj];
+      }
+      ii = i + 1, jj = j - 1;
+      if (ii <= m && jj >= 1) {
+        dp[i][j] += dp[ii][jj];
       }
     }
-  }  
+  }
   cout << dp[n][k] << endl;
   return 0;
 }
---
+++ src/olymp/cf/5/c/sol.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool ok(const string &s, int p, int q) {
+  int cnt = 0;
+  for (int i = p; i <= q; ++i) {
+    if (s[i] == '(') {
+      ++cnt;
+    } else {
+      --cnt;
+    }
+    if (cnt < 0) {
+      return false;
+    }
+  }
+  return cnt == 0;
+}
+
+int main() {
+  string s;
+  cin >> s;
+  int n = (int)s.size();
+  vector<vector<int>> dp(n + 1, vector<int>(n + 1));
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+    }
+  }
+  cout << endl;
+  return 0;
+}
