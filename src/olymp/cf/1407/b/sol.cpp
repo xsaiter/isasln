e335fcda21ae11a360a7251f4ddfc3b3d7f8d98a
xsaiter <xsaiter@mail.ru>
2025-12-15 23:23:15 +0300

    cf 1407 b done

--- src/olymp/cf/1407/b/sol.cpp
+++ src/olymp/cf/1407/b/sol.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 
-vector<int> solve(int n, vector<int> &a) {
+vector<int> solve(int n, const vector<int> &a) {
   vector<int> b;
+  vector<bool> used(n, false);
+  int x = 0;  
+  for (int i = 0; i < n; ++i) {
+    int best = -1;
+    int best_id = -1;
+    for (int j = 0; j < n; ++j) {
+      if (!used[j] && gcd(x, a[j]) > best) {
+        best = gcd(x, a[j]);
+        best_id = j;
+      }      
+    }
+    used[best_id] = true;
+    x = best;
+    b.push_back(a[best_id]);
+  }  
   return b;
 }
 
