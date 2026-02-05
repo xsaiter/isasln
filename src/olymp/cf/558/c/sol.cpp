1b6b337ffef150df4f643218d40feda788f1d0fc
xsaiter <xsaiter@mail.ru>
2026-02-05 22:33:35 +0300

    cf 558 c done

--- src/olymp/cf/558/c/sol.cpp
+++ src/olymp/cf/558/c/sol.cpp
@@ -4,38 +4,46 @@ using namespace std;
 
 const int INF = 1e9;
 
-int solve(int n, vector<int> &a) {  
-  int m = *(max_element(a.begin(), a.end())) + 1;      
+int solve(int n, vector<int>& a) {
+  int m = *(max_element(a.begin(), a.end())) + 1;
   m *= 2;
-  vector<int> b(m), c(m);  
-  for (int i = 0; i < n; ++i) {    
+  vector<int> b(m), c(m);
+  for (int i = 0; i < n; ++i) {
+    vector<bool> bb(m);    
     int x = a[i];
+    bb[x] = true;    
     c[x] += 1;
-    int step = 0;
-    while(1) {
-      x *= 2;      
-      if (x >= m) {
-        break;
+    int step_x = 0;
+    while (x >= 1) {
+      int y = x;
+      int step_y = step_x;
+      while (1) {
+        y *= 2;
+        if (y >= m) {
+          break;
+        }
+        if (!bb[y]) {
+          step_y++;
+          b[y] += step_y;
+          c[y] += 1;
+          bb[y] = true;
+        }
       }
-      step++;
-      b[x] += step;
-      c[x] += 1;
-    }
-    x = a[i];
-    step = 0;
-    while(x > 1) {
       x /= 2;
-      step++;
-      b[x] += step;
-      c[x] += 1;      
+      step_x++;
+      if (!bb[x]) {
+        b[x] += step_x;
+        c[x] += 1;
+        bb[x] = true;
+      }
     }
   }  
   int ans = INF;
-  for (int i = 0; i < m; ++i) {    
+  for (int i = 0; i < m; ++i) {
     if (c[i] == n) {
       ans = min(ans, b[i]);
     }
-  }  
+  }
   return ans;
 }
 
@@ -49,4 +57,3 @@ int main() {
   cout << solve(n, a) << endl;
   return 0;
 }
- 
