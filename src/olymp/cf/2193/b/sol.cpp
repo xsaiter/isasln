cf23eabc84012c9361f4af4a8e02826c3d224da6
xsaiter <xsaiter@mail.ru>
2026-02-16 12:51:42 +0300

    cf 2193 b done

--- src/olymp/cf/2193/b/sol.cpp
+++ src/olymp/cf/2193/b/sol.cpp
@@ -11,21 +11,23 @@ int main() {
     vector<int> a(n);
     for (int i = 0; i < n; ++i) {
       cin >> a[i];
-    }    
+    }
     int p = -1, q = -1;
     for (int i = 0; i < n; ++i) {
-      if (a[i] < n - i) {
+      if (a[i] != n - i) {
         p = i;
         break;
       }
-    }
-    for (int i = n - 1; i >= 0; --i) {
-      if (a[i] > n - i) {
-        q = i;
-        break;
+    }    
+    if (p != -1) {
+      for (int i = n - 1; i >= 0; --i) {
+        if (a[i] == n - p) {
+          q = i;
+          break;
+        }
       }
-    }
-    if (p != -1 && q != -1 && p <= q) {
+    }    
+    if (p != -1 && q != -1) {
       reverse(a.begin() + p, a.begin() + q + 1);
     }
     for (int i = 0; i < n; ++i) {
